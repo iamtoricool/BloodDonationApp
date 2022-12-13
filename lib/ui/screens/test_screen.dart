e88b139741a9861c0322a161988f5d752319a745
iamtoricool <iamtoricool@gmail.com>
2022-12-13 05:16:53 -0800

    Assets and theme color added

--- lib/ui/screens/test_screen.dart
+++ lib/ui/screens/test_screen.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Center(child: Text('Test Screen OK')),
-    );
+    return Scaffold();
   }
 }
