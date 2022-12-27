c8fc3e262fdc1babb47e843b1b5a95492cd2818a
iamtoricool <iamtoricool@gmail.com>
2022-12-27 11:41:27 -0500

    Authentication Screen UI Completed

--- lib/ui/screens/verify_screen.dart
+++ lib/ui/screens/verify_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+import 'package:blood_donation_app/ui/screens/add_new_password.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'package:blood_donation_app/ui/app_theme/theme_colors.dart';
 import 'package:blood_donation_app/ui/widget/custom_design.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nb_utils/nb_utils.dart';
 import 'package:pinput/pinput.dart';
 
 class VerifyScreen extends StatefulWidget {
@@ -13,6 +15,29 @@ class VerifyScreen extends StatefulWidget {
 }
 
 class _VerifyScreenState extends State<VerifyScreen> {
+  TextEditingController _pinController = (TextEditingController());
+  int start = 5;
+  void resendTimer() {
+    const onsec = Duration(seconds: 1);
+    Timer timer = Timer.periodic(onsec, (timer) {
+      if (start == 0) {
+        setState(() {
+          timer.cancel();
+        });
+      } else {
+        setState(() {
+          start--;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    resendTimer();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,22 +49,59 @@ class _VerifyScreenState extends State<VerifyScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Pinput(
-                  defaultPinTheme: PinTheme(
-                decoration: BoxDecoration(
-                    color: ThemeColors.greyColor.withOpacity(0.4),
-                    borderRadius: BorderRadius.circular(5)),
-                height: 65,
-                width: 65,
-                textStyle: GoogleFonts.poppins(
-                    fontSize: 40, color: ThemeColors.darkColor),
-              )),
+                controller: _pinController,
+                cursor: Container(
+                  height: 24,
+                  width: 2,
+                  color: ThemeColors.primaryColor,
+                ),
+                defaultPinTheme: PinTheme(
+                    height: 65,
+                    width: 65,
+                    textStyle: GoogleFonts.poppins(
+                        fontSize: 30, color: ThemeColors.darkColor),
+                    decoration: BoxDecoration(
+                        color: ThemeColors.greyColor.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(5))),
+              ),
               const SizedBox(
-                height: 20,
+                height: 16,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.centerRight,
+                child: start != 0
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Resend code in ',
+                            style: GoogleFonts.poppins(
+                                fontSize: 14, color: ThemeColors.primaryColor),
+                          ),
+                          Text(
+                            '$start Sec',
+                            style: GoogleFonts.poppins(
+                                fontSize: 14, color: ThemeColors.primaryColor),
+                          ),
+                        ],
+                      )
+                    : Text(
+                        'Send Code Again',
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, color: ThemeColors.primaryColor),
+                      ).onTap(() {}),
               ),
               CustomTextButton(
                 isFilled: true,
-                labelText: 'Send',
-                onPressed: () {},
+                labelText: 'Verify',
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AddNewPasswordScreen(),
+                      ));
+                },
               ),
               const SizedBox(
                 height: 15,
