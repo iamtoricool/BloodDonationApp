c77c4280608b2788f97ae069f29310f18a622d59
Mosaddik <mosaddikali58@gmail.com>
2023-01-16 18:56:06 +0600

    Slider add

--- lib/ui/Home Page/home_page.dart
+++ lib/ui/Home Page/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+import '../../Model/slider_file.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,7 +33,36 @@ class _HomePageState extends State<HomePage> {
                   child: Icon(Icons.notification_add_outlined),
                 )
               ],
-            )
+            ),
+            CarouselSlider(
+              items: [
+                ListView.builder(
+                  itemCount: slider.length,
+                  itemBuilder: (_, index) => Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage('${slider[index].imageUrl}'))),
+                  ),
+                )
+              ],
+              options: CarouselOptions(
+                height: 300.0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                // autoPlayInterval: Duration(milliseconds: 100),// default 400
+                // reverse: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                // scrollDirection: Axis.horizontal,
+                //or scrollDirection: Axis.vertical, default horizontal
+                viewportFraction: 0.8,
+                // padEnds: false first page show half
+              ),
+            ),
           ],
         ),
       ),
